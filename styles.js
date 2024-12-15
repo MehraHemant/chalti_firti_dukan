58ee77c74c6a159bbdc7e70182581cd614872c14
hemant <hemant>
2024-12-15 17:46:18 +0530

    feat: create services page

--- styles.js
+++ styles.js
@@ -16,10 +16,31 @@ document.head.appendChild(
         @apply w-20 h-20 rounded-full bg-white;
         }
         .primary-btn {
-        @apply uppercase border-2 hover:bg-white hover:text-black hover:border-black hover:shadow-2xl w-fit px-3 py-2 tracking-widest font-semibold flex items-center gap-3 mx-auto;
+        @apply uppercase border-2 hover:bg-white hover:text-black hover:border-black hover:shadow-2xl w-fit px-3 py-2 tracking-widest font-semibold flex items-center gap-3  duration-300 transition-all ease-in-out;
         }
       }
       `,
-     
   })
 );
+
+const services = document.getElementById("services");
+
+function handleNavItemClick(e) {
+  e.preventDefault();
+  console.log("handleNavItemClicked", e);
+  const dropdown = document.getElementById("dropdown");
+  dropdown.classList.toggle("hidden");
+}
+
+document.addEventListener('click', function handleClickOutsideBox(event) {
+  const dropdown = document.getElementById('dropdown');
+  const services = document.getElementById('services');
+  
+  if (
+    dropdown &&
+    !dropdown.contains(event.target) &&
+    (!services || !services.contains(event.target))
+  ) {
+    dropdown.classList.add('hidden');
+  }
+});
--- tailwind.js
+++ tailwind.js
@@ -1,9 +1,15 @@
 tailwind.config = {
     theme: {
       extend: {
+        screen:{
+          '7xl': '1940px',
+        },
         colors: {
           primary: '#CD0159',
           secondary: '#FF92C9'
+        },
+        fontFamily: {
+          'rubik-dirt': ['Rubik Dirt', 'system-ui']
         }
       }
     }
