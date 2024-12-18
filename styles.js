6a6542d83b21bb5d1a68ba1d8c3ce6ae51193ff5
hemant <hemant>
2024-12-18 13:56:14 +0530

    fix: css for homepage

--- styles.js
+++ styles.js
@@ -10,13 +10,22 @@ document.head.appendChild(
         @apply flex flex-col items-center px-4 py-5 bg-black/25 min-h-96;
         }
        .heading {
-         @apply text-center text-3xl underline uppercase font-semibold mb-6 underline-offset-4;
+         @apply pt-3 text-6xl font-extrabold uppercase max-lg:text-4xl max-lg:font-bold max-md:text-3xl max-md:font-extrabold max-sm:text-2xl/5;
         }
         .avatar {
-        @apply w-20 h-20 rounded-full bg-white;
+         @apply w-20 h-20 rounded-full bg-white;
+        }
+        .card-heading {
+         @apply text-4xl font-semibold max-md:text-2xl/10 max-sm:text-xl/6 uppercase;
+        }
+        .card-desc {
+         @apply px-3 text-center text-lg max-sm:text-base/5;
         }
         .primary-btn {
-        @apply uppercase border-2 hover:bg-white hover:text-black hover:border-black hover:shadow-2xl w-fit px-3 py-2 tracking-widest font-semibold flex items-center gap-3  duration-300 transition-all ease-in-out;
+         @apply uppercase border-2 hover:bg-[#FF92C9] hover:border-black hover:shadow-2xl w-fit px-3 py-2 tracking-widest font-semibold flex items-center gap-3  duration-300 transition-all ease-in-out;
+        }
+        .input-field{
+        @apply bg-secondary border-primary placeholder:text-primary/60 w-full rounded-lg px-3 py-2 outline-none max-sm:bg-secondary/20 max-sm:rounded-none max-sm:border-b max-sm:border-secondary max-sm:placeholder:text-secondary
         }
       }
       `,
